68fa563285558f767d4cad6b5e443a5cde92b9a7
carsonmcmahan <carson.mcmahan@gmail.com>
2026-02-20 11:17:00 -0700

    wrote three functions and tests for each of them

--- shopping-cart/src/cart.test.ts
+++ shopping-cart/src/cart.test.ts
@@ -8,8 +8,33 @@ describe("ShoppingCart", () => {
     cart = new ShoppingCart();
   });
 
+  /* BEGIN ADD ITEM TESTS */
   it("should start with an empty cart", () => {
     expect(cart.items).toEqual([]);
+  });
+
+  it("should not add an item that has a price of 0 or negative", () => {
+    // adding an item with a price of 0
+    expect(() => {
+      cart.addItem({ id: 1, name: "Apple", price: 0, quantity: 2 });
+    }).toThrowError("Item price must be greater than zero");
+
+    // adding an item with a price that is negative
+    expect(() => {
+      cart.addItem({ id: 2, name: "Orange", price: -9, quantity: 1 });
+    }).toThrowError("Item price must be greater than zero");
+  });
+
+  it("should not add an item that has a quantity of 0 or negative", () => {
+    // adding an item with a quantity of 0
+    expect(() => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 0 });
+    }).toThrowError("Item quantity must be greater than zero");
+
+    // adding an item with a quantity that is negative
+    expect(() => {
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: -10 });
+    }).toThrowError("Item quantity must be greater than zero");
   });
 
   it("should add an item to the cart", () => {
@@ -18,4 +43,47 @@ describe("ShoppingCart", () => {
     expect(cart.items.length).toBe(1);
     expect(cart.items[0]?.name).toBe("Apple");
   });
+
+  it("should update an items quantity if it exists", () => {
+    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 1 });
+    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 1 });
+
+    expect(cart.items.length).toBe(1);
+    expect(cart.items[0]?.quantity).toBe(2);
+  });
+  /* END ADD ITEM TESTS */
+
+  /* BEGIN REMOVE ITEM TESTS */
+  it("should remove an item completely form the cart", () => {
+    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+    cart.addItem({ id: 3, name: "Bread", price: 2.5, quantity: 1 });
+    cart.addItem({ id: 4, name: "Beans", price: 0.99, quantity: 2 });
+
+    cart.removeItem(2);
+    expect(cart.items.length).toBe(3);
+    expect(cart.items[1]?.id).toBe(3);
+  });
+
+  it("should exit removeItem function if item does not exist", () => {
+    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+    cart.addItem({ id: 3, name: "Bread", price: 2.5, quantity: 1 });
+    cart.addItem({ id: 4, name: "Beans", price: 0.99, quantity: 2 });
+
+    expect(() => {
+      cart.removeItem(5);
+    }).toThrowError("Item must exist to be removed");
+  });
+  /* END REMOVE ITEM TESTS */
+
+  /* BEGIN GET SUBTOTAL TESTS */
+  it("should return the total price of all items before a discount", () => {
+    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+
+    const actual = cart.getSubtotal();
+    expect(actual).toBe(3.08);
+  });
+  /* END GET SUBTOTAL TESTS */
 });
--- shopping-cart/src/cart.ts
+++ shopping-cart/src/cart.ts
@@ -9,6 +9,35 @@ export class ShoppingCart {
   public items: CartItem[] = [];
 
   addItem(item: CartItem): void {
+    if (item.price <= 0) throw Error("Item price must be greater than zero");
+    if (item.quantity <= 0) throw Error("Item quantity must be greater than zero");
+
+    const existingItem = this.items.find((i) => i.id === item.id);
+    if (existingItem) {
+      existingItem.quantity++;
+      return;
+    }
+
     this.items.push(item);
+  }
+
+  removeItem(id: number): void {
+    const itemToRemove = this.items.find((i) => i.id === id);
+    if (!itemToRemove) throw Error("Item must exist to be removed");
+
+    const index = this.items.indexOf(itemToRemove);
+    this.items.splice(index, 1);
+  }
+
+  getSubtotal(): number {
+    let total = 0;
+
+    this.items.forEach((item) => {
+      const itemPrice = item.quantity * item.price;
+      total += itemPrice;
+    });
+
+    // round to two decimal places
+    return Math.round(total * 100) / 100;
   }
 }
