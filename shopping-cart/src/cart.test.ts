4ccc851a61f4f17a3178fc3e2aa2c32046bf9366
Carson McMahan <carson.mcmahan@gmail.com>
2026-02-20 13:03:15 -0700

    shopping cart class and tests have been completed

--- shopping-cart/src/cart.test.ts
+++ shopping-cart/src/cart.test.ts
@@ -8,82 +8,134 @@ describe("ShoppingCart", () => {
     cart = new ShoppingCart();
   });
 
-  /* BEGIN ADD ITEM TESTS */
-  it("should start with an empty cart", () => {
-    expect(cart.items).toEqual([]);
+  describe("addItem()", () => {
+    it("should start with an empty cart", () => {
+      expect(cart.items).toEqual([]);
+    });
+
+    it("should not add an item that has a price of 0 or negative", () => {
+      expect(() => {
+        cart.addItem({ id: 1, name: "Apple", price: 0, quantity: 2 });
+      }).toThrowError("Item price must be greater than zero");
+
+      expect(() => {
+        cart.addItem({ id: 2, name: "Orange", price: -9, quantity: 1 });
+      }).toThrowError("Item price must be greater than zero");
+    });
+
+    it("should not add an item that has a quantity of 0 or negative", () => {
+      expect(() => {
+        cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 0 });
+      }).toThrowError("Item quantity must be greater than zero");
+
+      expect(() => {
+        cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: -10 });
+      }).toThrowError("Item quantity must be greater than zero");
+    });
+
+    it("should add an item to the cart", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 2 });
+
+      expect(cart.items.length).toBe(1);
+      expect(cart.items[0]?.name).toBe("Apple");
+    });
+
+    it("should update an items quantity if it exists", () => {
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 2 });
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+
+      expect(cart.items.length).toBe(1);
+      expect(cart.items[0]?.quantity).toBe(5);
+    });
   });
 
-  it("should not add an item that has a price of 0 or negative", () => {
-    // adding an item with a price of 0
-    expect(() => {
-      cart.addItem({ id: 1, name: "Apple", price: 0, quantity: 2 });
-    }).toThrowError("Item price must be greater than zero");
+  describe("removeItem()", () => {
+    it("should remove an item completely form the cart", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+      cart.addItem({ id: 3, name: "Bread", price: 2.5, quantity: 1 });
+      cart.addItem({ id: 4, name: "Beans", price: 0.99, quantity: 2 });
 
-    // adding an item with a price that is negative
-    expect(() => {
-      cart.addItem({ id: 2, name: "Orange", price: -9, quantity: 1 });
-    }).toThrowError("Item price must be greater than zero");
+      cart.removeItem(2);
+      expect(cart.items.length).toBe(3);
+      expect(cart.items[1]?.id).toBe(3);
+    });
+
+    it("should exit removeItem function if item does not exist", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+
+      expect(() => {
+        cart.removeItem(5);
+      }).toThrowError("Item must exist to be removed");
+    });
   });
 
-  it("should not add an item that has a quantity of 0 or negative", () => {
-    // adding an item with a quantity of 0
-    expect(() => {
-      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 0 });
-    }).toThrowError("Item quantity must be greater than zero");
+  describe("getSubtotal()", () => {
+    it("should return the total price of all items before a discount", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+      cart.addItem({ id: 3, name: "Grapes", price: 1.5, quantity: 5 });
 
-    // adding an item with a quantity that is negative
-    expect(() => {
-      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: -10 });
-    }).toThrowError("Item quantity must be greater than zero");
+      const actual = cart.getSubtotal();
+      expect(actual).toBe(10.58);
+    });
   });
 
-  it("should add an item to the cart", () => {
-    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 2 });
+  describe("applyPromoCode()", () => {
+    it("should not apply discount is code does not exist", () => {
+      const given = "SAVE50";
 
-    expect(cart.items.length).toBe(1);
-    expect(cart.items[0]?.name).toBe("Apple");
+      expect(() => {
+        cart.applyPromoCode(given);
+      }).toThrowError("Must use a valid discount code");
+    });
+
+    it("should take a discount code and store the percentage in a variable", () => {
+      const given = "SAVE30";
+      cart.applyPromoCode(given);
+
+      expect(cart.discountPercentage).toBe(0.3);
+    });
   });
 
-  it("should update an items quantity if it exists", () => {
-    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 1 });
-    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 1 });
+  describe("getGrandTotal()", () => {
+    it("should return the subtotal if there is no discount", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+      cart.addItem({ id: 3, name: "Grapes", price: 1.5, quantity: 5 });
 
-    expect(cart.items.length).toBe(1);
-    expect(cart.items[0]?.quantity).toBe(2);
+      const actual = cart.getGrandTotal();
+      expect(actual).toBe(10.58);
+    });
+
+    it("should return the subtotal minus the active discount percentage", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+      cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
+      cart.addItem({ id: 3, name: "Grapes", price: 1.5, quantity: 5 });
+
+      const promoCode = "SAVE30";
+      cart.applyPromoCode(promoCode);
+
+      const actual = cart.getGrandTotal();
+      expect(actual).toBe(7.41);
+    });
   });
-  /* END ADD ITEM TESTS */
 
-  /* BEGIN REMOVE ITEM TESTS */
-  it("should remove an item completely form the cart", () => {
-    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
-    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
-    cart.addItem({ id: 3, name: "Bread", price: 2.5, quantity: 1 });
-    cart.addItem({ id: 4, name: "Beans", price: 0.99, quantity: 2 });
+  describe("clearCart()", () => {
+    it("should throw error if cart is already empty and no active discount", () => {
+      expect(() => {
+        cart.clearCart();
+      }).toThrowError("Cart is already empty and no discount applied");
+    });
 
-    cart.removeItem(2);
-    expect(cart.items.length).toBe(3);
-    expect(cart.items[1]?.id).toBe(3);
+    it("should clear cart array and remove active discount", () => {
+      cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
+      cart.applyPromoCode("SAVE10");
+
+      cart.clearCart();
+
+      expect(cart.items).toEqual([]);
+      expect(cart.discountPercentage).toEqual(undefined);
+    });
   });
-
-  it("should exit removeItem function if item does not exist", () => {
-    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
-    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
-    cart.addItem({ id: 3, name: "Bread", price: 2.5, quantity: 1 });
-    cart.addItem({ id: 4, name: "Beans", price: 0.99, quantity: 2 });
-
-    expect(() => {
-      cart.removeItem(5);
-    }).toThrowError("Item must exist to be removed");
-  });
-  /* END REMOVE ITEM TESTS */
-
-  /* BEGIN GET SUBTOTAL TESTS */
-  it("should return the total price of all items before a discount", () => {
-    cart.addItem({ id: 1, name: "Apple", price: 0.68, quantity: 1 });
-    cart.addItem({ id: 2, name: "Orange", price: 0.8, quantity: 3 });
-
-    const actual = cart.getSubtotal();
-    expect(actual).toBe(3.08);
-  });
-  /* END GET SUBTOTAL TESTS */
 });
--- shopping-cart/src/cart.ts
+++ shopping-cart/src/cart.ts
@@ -6,38 +6,77 @@ export interface CartItem {
 }
 
 export class ShoppingCart {
-  public items: CartItem[] = [];
+  private _items: CartItem[] = [];
+  private _discountPercentage: number | undefined;
+
+  private discounts: Record<string, number> = {
+    SAVE10: 10,
+    SAVE15: 15,
+    SAVE20: 20,
+    SAVE30: 30,
+  };
+
+  get items(): CartItem[] {
+    return this._items;
+  }
+
+  get discountPercentage(): number | undefined {
+    return this._discountPercentage;
+  }
 
   addItem(item: CartItem): void {
     if (item.price <= 0) throw Error("Item price must be greater than zero");
     if (item.quantity <= 0) throw Error("Item quantity must be greater than zero");
 
-    const existingItem = this.items.find((i) => i.id === item.id);
+    const existingItem = this._items.find((i) => i.id === item.id);
     if (existingItem) {
-      existingItem.quantity++;
+      existingItem.quantity += item.quantity;
       return;
     }
 
-    this.items.push(item);
+    this._items.push(item);
   }
 
   removeItem(id: number): void {
-    const itemToRemove = this.items.find((i) => i.id === id);
+    const itemToRemove = this._items.find((i) => i.id === id);
     if (!itemToRemove) throw Error("Item must exist to be removed");
 
-    const index = this.items.indexOf(itemToRemove);
-    this.items.splice(index, 1);
+    const index = this._items.indexOf(itemToRemove);
+    this._items.splice(index, 1);
   }
 
   getSubtotal(): number {
-    let total = 0;
+    const total = this._items.reduce((sum, item) => {
+      return sum + item.quantity * item.price;
+    }, 0);
 
-    this.items.forEach((item) => {
-      const itemPrice = item.quantity * item.price;
-      total += itemPrice;
-    });
-
-    // round to two decimal places
     return Math.round(total * 100) / 100;
+  }
+
+  applyPromoCode(code: string) {
+    const discount = this.discounts[code];
+    if (!discount) throw Error("Must use a valid discount code");
+
+    this._discountPercentage = discount / 100;
+  }
+
+  getGrandTotal(): number {
+    const subtotal = this.getSubtotal();
+
+    if (this._discountPercentage === undefined) {
+      return subtotal;
+    }
+
+    const cartPrice = subtotal - subtotal * this._discountPercentage;
+    return Math.round(cartPrice * 100) / 100;
+  }
+
+  clearCart() {
+    if (this._items.length === 0 && this._discountPercentage === undefined) {
+      throw Error("Cart is already empty and no discount applied");
+    }
+
+    this._items = [];
+    this._discountPercentage = undefined;
   }
 }
